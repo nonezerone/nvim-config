de21f3333f6e6221c10c9aaf9d89cc251226af62
nonezerone <koremailme@gmail.com>
2024-07-09 09:23:18 +0400

    feat: cleanup config a little

--- lua/nonezerone/lazy/colors.lua
+++ lua/nonezerone/lazy/colors.lua
@@ -1,20 +1,18 @@
-function GetColourHere(color)
-	color = color or "kanagawa-dragon"
-	vim.cmd.colorscheme(color)
-end
-
 return {
-	{
-		"rebelot/kanagawa.nvim",
-        config = function()
-            require("kanagawa").setup({
-                commentStyle = { italic = false },
-                functionStyle = { italic = false },
-                keywordStyle = { italic = false },
-                statementStyle = { bold = true },
-                theme = "dragon",
-            })
-            GetColourHere()
-        end
-    }
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+
+    config = function()
+        require("kanagawa").setup({
+            commentStyle = { italic = false },
+            functionStyle = { italic = false },
+            keywordStyle = { italic = false },
+            statementStyle = { bold = true },
+            theme = "dragon",
+        })
+    end,
+
+    init = function()
+        vim.cmd.colorscheme("kanagawa-dragon")
+    end,
 }
--- lua/nonezerone/lazy/treesitter.lua
+++ lua/nonezerone/lazy/treesitter.lua
@@ -3,27 +3,15 @@ return {
   build = ":TSUpdate",
   config = function()
       require("nvim-treesitter.configs").setup({
-          -- A list of parser names, or "all"
-          ensure_installed = {
-              "vimdoc", "javascript", "typescript", "c", "lua", "rust", "jsdoc", "bash", "ruby"
-          },
-
+          ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "rust", "jsdoc", "bash", "ruby" },
           sync_install = false,
-
           auto_install = true,
 
-          indent = {
-              enable = true,
-              disable = { "ruby" },
-          },
+          indent = { enable = true, disable = { "ruby" }, },
 
           highlight = {
               enable = true,
               additional_vim_regex_highlighting = { "ruby" },
-          },
-
-          endwise = {
-              enable = true,
           },
       })
   end
