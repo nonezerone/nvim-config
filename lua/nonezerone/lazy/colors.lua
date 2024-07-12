56ebe98d1ff5cdb11be081aa1542e16bb2e9111a
nonezerone <koremailme@gmail.com>
2024-07-12 16:47:06 +0400

    feat: add rose-pine scheme

--- lua/nonezerone/lazy/colors.lua
+++ lua/nonezerone/lazy/colors.lua
@@ -1,18 +1,34 @@
 return {
-    "rebelot/kanagawa.nvim",
-    priority = 1000,
 
-    config = function()
-        require("kanagawa").setup({
-            commentStyle = { italic = false },
-            functionStyle = { italic = false },
-            keywordStyle = { italic = false },
-            statementStyle = { bold = true },
-            theme = "dragon",
-        })
-    end,
+    {
+        "rebelot/kanagawa.nvim",
+        priority = 1000,
 
-    init = function()
-        vim.cmd.colorscheme("kanagawa-dragon")
-    end,
+        config = function()
+            require("kanagawa").setup({
+                commentStyle = { italic = false },
+                functionStyle = { italic = false },
+                keywordStyle = { italic = false },
+                statementStyle = { bold = true },
+                theme = "dragon",
+            })
+        end,
+
+        init = function()
+            vim.cmd.colorscheme("kanagawa-dragon")
+        end,
+    },
+
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require('rose-pine').setup({
+                styles = {
+                    italic = false,
+                },
+            })
+        end,
+
+    }
 }
